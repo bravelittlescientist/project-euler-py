8c09c8886f8f14d4c3acde8ec6d3663f505e9073
Eugenia Gabrielova <eugenia.g@uci.edu>
2011-12-17 23:33:34 -0800

    Problem 66: Wrote test cases, 3/3 fail (no method implementations).

--- python/src/Set_51_to_75/Problem_66.py
+++ python/src/Set_51_to_75/Problem_66.py
@@ -26,12 +26,37 @@
 import unittest
 
 class Test_Problem_66(unittest.TestCase):
-	pass
+	
+	def test_minimal_diophantine_solution_in_x(self):
+		""" Compute solutions minimal in x """
+		self.assertEqual(3, minimal_diophantine_solution_in_x(2))
+		self.assertEqual(2, minimal_diophantine_solution_in_x(3))
+		self.assertEqual(9, minimal_diophantine_solution_in_x(5))
+		self.assertEqual(5, minimal_diophantine_solution_in_x(6))
+		self.assertEqual(8, minimal_diophantine_solution_in_x(7))
+		self.assertEqual(649, minimal_diophantine_solution_in_x(13))
+	
+	def test_no_solution_when_D_is_square(self):
+		""" No solutions in positive integers when D is square """
+		self.assertEqual(-1, minimal_diophantine_solution_in_x(4))
+
+	def test_value_of_D_in_minimal_solutions_of_x_for_largest_x(self):
+		""" Considering minimal solutions in x for D <= threshold, find
+		D for which largest x is obtained
+		"""
+		self.assertEqual(5, optimal_diophantine_minimal_in_x(7))
+
+def minimal_diophantine_solution_in_x(D):
+	""" Compute minimal diophantine solution in x, given D """
+	return 0
+
+def optimal_diophantine_minimal_in_x(upper_limit):
+	""" Find D for which largest X is obtianed, for diophantine solutions
+	minimal in x
+	"""
+	return 0
 
 if __name__ == '__main__':
 	# Run unit tests
 	suite = unittest.TestLoader().loadTestsFromTestCase(Test_Problem_66)
 	unittest.TextTestRunner(verbosity=2).run(suite)
-
-	
-	
