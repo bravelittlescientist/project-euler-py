bccdeed9278ffdd0e583e0554a44a4c415f78b23
Eugenia Gabrielova <eugenia.g@uci.edu>
2011-12-24 15:42:37 -0800

    Problem 40: Passing generator test cases, modified main comp with multiplier.

--- python/src/Set_26_to_50/Problem_40.py
+++ python/src/Set_26_to_50/Problem_40.py
@@ -17,25 +17,39 @@ class Test_Problem_40(unittest.TestCase):
 	def test_nth_digit_of_fractional_part(self):
 		""" Check that nth digits are correctly calculated """
 		counter = 1
-		for digit in generate_digits(20):
+		for digit in generate_digits(13):
 			if counter == 1 or counter == 10 or counter == 12:
 				self.assertEqual(1, digit)
 		
 			counter += 1
 
-	def test_computes_max_nth_digit_by_multiples_of_10(self):
+	def test_computes_max_nth_digit_by_multiples(self):
 		""" Ensure that generator knows when to stop """
-		self.assertEqual(1, products_of_multiple_of_ten_nth_digits(100000))
+		self.assertEqual(64, products_of_multiplied_nth_digits(20, 2))
+		self.assertEqual(1, products_of_multiplied_nth_digits(1000000, 10))
 
 def generate_digits(maximum):
 	""" Generates digit of irrational decimal fraction """ 
-	yield 0
+	counter = 1
+	while counter < maximum:
+		for digit in list(str(counter)):
+			yield int(digit)
+			
+		counter += len(list(str(counter)))
 
-def products_of_multiple_of_ten_nth_digits(maximum):
-	""" Computes product of d1, d10 ... dmaximum via multiples of 10
+def products_of_multiplied_nth_digits(maximum, multiplier):
+	""" Computes product of d1, d10 ... dmaximum via multiples of multiplier
 
 	Assuming nth maximum is some 1 x 10^x """
-	return 0
+	# Initialize product and multiplier level
+	product = 1
+	level = 1
+
+	# Read values from generator, multiplying product by value
+	# if level reached
+	
+
+	return product
 
 if __name__ == '__main__':
 	suite = unittest.TestLoader().loadTestsFromTestCase(Test_Problem_40)
