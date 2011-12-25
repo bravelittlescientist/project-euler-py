ecc9ee70f16de647bd52a1692138c30a7fe04939
Eugenia Gabrielova <eugenia.g@uci.edu>
2011-12-24 17:17:38 -0800

    Problem 40 Solved: Fixed Generator Bug returning incorrect digits.

--- python/src/Set_26_to_50/Problem_40.py
+++ python/src/Set_26_to_50/Problem_40.py
@@ -23,19 +23,24 @@ class Test_Problem_40(unittest.TestCase):
 		
 			counter += 1
 
-	def test_computes_max_nth_digit_by_multiples(self):
-		""" Ensure that generator knows when to stop """
+	def test_computes_max_nth_digit_by_multiples_of_two(self):
+		""" Ensure that generator knows when to stop """		
 		self.assertEqual(64, products_of_multiplied_nth_digits(20, 2))
-		self.assertEqual(1, products_of_multiplied_nth_digits(1000000, 10))
+
+	def test_computes_max_nth_digit_by_multiples_of_ten(self):
+		""" Ensure that generator computes multiples of 10 for small max """
+		self.assertEqual(1, products_of_multiplied_nth_digits(20, 10))
 
 def generate_digits(maximum):
 	""" Generates digit of irrational decimal fraction """ 
 	counter = 1
-	while counter < maximum:
+	digitcount = 1
+	while digitcount <= maximum:
 		for digit in list(str(counter)):
 			yield int(digit)
 			
-		counter += len(list(str(counter)))
+		counter += 1
+		digitcount += len(list(str(counter)))
 
 def products_of_multiplied_nth_digits(maximum, multiplier):
 	""" Computes product of d1, d10 ... dmaximum via multiples of multiplier
@@ -47,10 +52,21 @@ def products_of_multiplied_nth_digits(maximum, multiplier):
 
 	# Read values from generator, multiplying product by value
 	# if level reached
-	
+	counter = 1
+	for digit in generate_digits(maximum):
+		# If counter reaches level, update level with multiplier
+		# and update product with current digit
+		if counter == level:
+			product *= digit
+			level *= multiplier
+
+		# Increment digit counter
+		counter += 1
 
 	return product
 
 if __name__ == '__main__':
 	suite = unittest.TestLoader().loadTestsFromTestCase(Test_Problem_40)
 	unittest.TextTestRunner(verbosity=2).run(suite)
+
+	print "Result:", products_of_multiplied_nth_digits(1000000, 10)
